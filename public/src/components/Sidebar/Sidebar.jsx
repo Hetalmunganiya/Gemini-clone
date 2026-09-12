1d66e9f8a2466c00cf0d400ec0a1b6e2d5a58ecb
Hetal Munganiya <jainhetal504@gmail.com>
2026-09-12 21:19:16 +0530

    make layout and sidebar fully responsive

--- public/src/components/Sidebar/Sidebar.jsx
+++ public/src/components/Sidebar/Sidebar.jsx
@@ -1,33 +1,44 @@
 import React, { useContext, useState } from 'react'
 import './Sidebar.css'
-import {assets} from '../../assets/assets'
+import { assets } from '../../assets/assets'
 import { Context } from '../../context/context'
 
 const Sidebar = () => {
-
-  const [extended,setExtended] = useState(false)
-  const {onSent,prevPrompts,setRecentPrompt,newChat,deletePrompt,clearAllPrompts,onSelectHistory} = useContext(Context)
+  const [extended, setExtended] = useState(false)
+  const { onSent, prevPrompts, setRecentPrompt, newChat, deletePrompt, clearAllPrompts } = useContext(Context)
 
   const loadPrompt = async (prompt) => {
     setRecentPrompt(prompt)
     await onSent(prompt)
+    if (window.innerWidth <= 600) {
+      setExtended(false)
+    }
   }
+
   return (
-    <div className="sidebar">
-        <div className="top">
-          <img onClick={()=>setExtended(prev=>!prev)} className='menu' src={assets.menu_icon} alt="" />
-          <div onClick={()=>{
-            newChat();
-            setExtended(true);
-          }}className="new-chat">
-            <img src={assets.plus_icon} alt="" />
-            {extended?<p>New chat</p>:null}
-          </div>
-          {extended
-          ?<div className="recent">
+    <div className={`sidebar ${extended ? "extended" : ""}`}>
+      <div className="top">
+        <img 
+          onClick={() => setExtended(prev => !prev)} 
+          className="menu" 
+          src={assets.menu_icon} 
+          alt="menu" 
+        />
+        <div 
+          onClick={() => {
+            newChat()
+            setExtended(true)
+          }} 
+          className="new-chat"
+        >
+          <img src={assets.plus_icon} alt="new chat" />
+          {extended ? <p>New chat</p> : null}
+        </div>
+        {extended ? (
+          <div className="recent">
             <p className="recent-title">Recent</p>
 
-            {prevPrompts.length>0 && (
+            {prevPrompts.length > 0 && (
               <p
                 onClick={clearAllPrompts}
                 style={{
@@ -52,33 +63,31 @@ const Sidebar = () => {
                 </div>
 
                 <img
-                  src={assets.close_icon} 
+                  src={assets.close_icon}
                   alt="delete"
-                  className= "close-icon"
+                  className="close-icon"
                   onClick={() => deletePrompt(index)}
-                  
                 />
               </div>
             ))}
           </div>
-         : null}
+        ) : null}
       </div>
 
-        <div className="bottom">
-          <div className="bottom-item recent-entry">
-            <img src={assets.question_icon} alt="" />
-            {extended?<p>Help</p>:null}
-          </div>
-          <div className="bottom-item recent-entry">
-            <img src={assets.history_icon} alt="" />
-            {extended?<p>Activity</p>:null}
-          </div>
-          <div className="bottom-item recent-entry">
-            <img src={assets.setting_icon} alt="" />
-            {extended?<p>Settings</p>:null}
-          </div>
+      <div className="bottom">
+        <div className="bottom-item recent-entry">
+          <img src={assets.question_icon} alt="help" />
+          {extended ? <p>Help</p> : null}
         </div>
-        
+        <div className="bottom-item recent-entry">
+          <img src={assets.history_icon} alt="activity" />
+          {extended ? <p>Activity</p> : null}
+        </div>
+        <div className="bottom-item recent-entry">
+          <img src={assets.setting_icon} alt="settings" />
+          {extended ? <p>Settings</p> : null}
+        </div>
+      </div>
     </div>
   )
 }
